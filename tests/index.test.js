075b1cec9da23ae07a625122664588fd0e07f578
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-08-24 18:12:43 +0300

    Update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,23 @@
 import fs from 'fs-extra'
 import { rollup } from 'rollup'
+import { terser } from 'rollup-plugin-terser'
 import generateHtml from '../src'
 
 process.chdir(`${__dirname}/fixtures`)
 
-async function build(options) {
+async function build(options, minify) {
   const bundle = await rollup({
     input: 'src/index.js',
     plugins: [
-      generateHtml(options)
+      generateHtml(options),
+      minify && terser()
     ]
   })
 
   await bundle.write({
     file: 'dist/app.js',
-    format: 'iife'
+    format: 'iife',
+    compact: minify
   })
 }
 
@@ -22,42 +25,72 @@ function readFile(filename) {
   return fs.readFile(filename, 'utf8')
 }
 
-
 afterEach(async () => {
   await fs.remove('dist')
 })
 
-test('Throw if "filename" is not set', async () => {
-  await expect(build({
-    template: 'src/template.html'
-  })).rejects.toThrow('"filename" is required')
-})
-
-test('Throw if specified template doesn\'t exist', async () => {
-  await expect(build({
-    filename: 'dist/index.html',
-    template: 'nonexistent/template.html'
-  })).rejects.toThrow('ENOENT: no such file or directory, open \'nonexistent/template.html\'')
-})
-
-test('Generate default template with injected script', async () => {
-  await build({
-    filename: 'dist/index.html'
+describe('Options', () => {
+  test('Throw if "filename" is not set', async () => {
+    await expect(build()).rejects.toThrow('"filename" is required')
   })
 
-  expect(await fs.pathExists('dist/app.js')).toBe(true)
-  expect(await fs.pathExists('dist/index.html')).toBe(true)
-  expect(await readFile('dist/index.html')).toBe(await readFile('samples/bare.html'))
+  test('Throw if template file doesn\'t exist', async () => {
+    await expect(build({
+      filename: 'dist/index.html',
+      template: 'nonexistent/template.html'
+    })).rejects.toThrow('ENOENT: no such file or directory, open \'nonexistent/template.html\'')
+  })
 })
 
-test('Generate default template with inline script', async () => {
-  await build({
-    filename: 'dist/index.html',
-    inline: true,
-    selector: 'head'
+describe('Default template', () => {
+  describe('External', () => {
+    test('Head', async () => {
+      await build({
+        filename: 'dist/index.html',
+        selector: 'head'
+      })
+
+      expect(await fs.pathExists('dist/app.js')).toBe(true)
+      expect(await fs.pathExists('dist/index.html')).toBe(true)
+      expect(await readFile('dist/index.html')).toBe(await readFile('samples/external-head.html'))
+    })
+
+    test('Body', async () => {
+      await build({
+        filename: 'dist/index.html',
+        selector: 'body'
+      })
+
+      expect(await fs.pathExists('dist/app.js')).toBe(true)
+      expect(await fs.pathExists('dist/index.html')).toBe(true)
+      expect(await readFile('dist/index.html')).toBe(await readFile('samples/external-body.html'))
+    })
   })
 
-  expect(await fs.pathExists('dist/app.js')).toBe(false)
-  expect(await fs.pathExists('dist/index.html')).toBe(true)
-  expect(await readFile('dist/index.html')).toBe(await readFile('samples/inline.html'))
+  describe('Inline', () => {
+    test('Unformatted', async () => {
+      await build({
+        filename: 'dist/index.html',
+        selector: 'head',
+        inline: true
+      }, true)
+
+      expect(await fs.pathExists('dist/app.js')).toBe(false)
+      expect(await fs.pathExists('dist/index.html')).toBe(true)
+      expect(await readFile('dist/index.html')).toBe(await readFile('samples/inline-unformatted.html'))
+    })
+
+    test('Formatted', async () => {
+      await build({
+        filename: 'dist/index.html',
+        selector: 'head',
+        inline: true,
+        formatInline: true
+      })
+
+      expect(await fs.pathExists('dist/app.js')).toBe(false)
+      expect(await fs.pathExists('dist/index.html')).toBe(true)
+      expect(await readFile('dist/index.html')).toBe(await readFile('samples/inline-formatted.html'))
+    })
+  })
 })
